e03ec8b99ca9553758e41110796622adb4c29360
vicdem989 <vicdemhoy@hotmail.com>
2024-04-29 21:23:49 +0200

    final addings

--- obj/Debug/net7.0/WeatherApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/WeatherApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9885071f5f8838e440a0f428486699c5ea2a23d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb09ee247b8fc26910cf96c23f423935dc603244")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
